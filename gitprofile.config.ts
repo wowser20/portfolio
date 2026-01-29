154eafe3e3c1b3162afb48e77364ae948ffce6f1
Andrea Ella Remoreras <ae.remoreras@gmail.com>
2026-01-29 19:50:35 +0800

    feat: update profile info

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,112 +2,106 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'wowser20', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/portfolio/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 2, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
+          forks: true, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['wowser20/go-rest-proxy', 'Nuxify/celeste'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
-      header: 'My Projects',
+      header: 'My Projects & Projects I\'ve Contributed To',
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Forward Proxy Server using Go',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Simple forward proxy server for securing HTTP requests.',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: 'https://github.com/wowser20/go-rest-proxy',
         },
         {
-          title: 'Project Name',
+          title: 'Nuxie: An AI-Powered Chat Platform Using LLM & RAG Technology',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Developed the APIs for user onboarding and connected the platform with various LLMs.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://nuxify.tech/images/case-studies/nuxie/laptop-5.webp',
+          link: 'https://hello-nuxie.nuxify.tech/',
         },
+        {
+          title: 'Certifika: A Multi-Chain NFT-Based Certificate Based Event Certification Platform',
+          description:
+            'Developed the APIs and integrated smartcontracts.',
+          imageUrl:
+            'https://nuxify.tech/images/case-studies/certifika/laptop-1.webp',
+          link: 'https://certifika.org/',
+        },
+        {
+          title: 'Decentralized and Instant Voting Platform Powered By Ethereum',
+          description:
+            'Due to NDA agreement I could not disclose the actual name of the project. Developed the APIs and integrated smartcontract.',
+          imageUrl:
+            'https://nuxify.tech/images/case-studies/decentralized-voting/laptop-2.webp',
+          link: 'https://nuxify.tech/works/decentralized-voting',
+        }
       ],
     },
   },
-  seo: { title: 'Portfolio of Ariful Alam', description: '', imageURL: '' },
+  seo: { title: 'Portfolio of Andrea Ella Remoreras', description: 'Backend Developer', imageURL: '' },
   social: {
-    linkedin: 'ariful-alam',
-    x: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
-    facebook: '',
-    instagram: '',
-    reddit: '',
-    threads: '',
-    youtube: '', // example: 'pewdiepie'
-    udemy: '',
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    discord: '',
-    telegram: '',
-    website: 'https://www.arifszn.com',
-    phone: '',
-    email: '',
+    linkedin: 'andrea-ella-remoreras-759a051b9',
+    dev: 'wowser20',
+    website: 'https://github.com/wowser20/portfolio',
+    phone: '09070841101',
+    email: 'ae.remoreras.dev@gmail.com',
   },
   resume: {
     fileUrl:
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
+    'Go',
+    'MySQL',
+    'JavaScript',,
     'CSS',
-    'Antd',
-    'Tailwind',
+    'Java',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Nuxify Inc.',
+      position: 'Software Engineer',
+      from: 'September 2023',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://nuxify.tech/',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Department of Science & Technology',
+      position: 'Full Stack Developer Intern',
+      from: 'July 2022',
+      to: 'August 2022',
+      companyLink: 'https://www.stii.dost.gov.ph/',
     },
   ],
   certifications: [
@@ -152,12 +146,6 @@ const CONFIG = {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
   ],
-  // Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
-  },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
@@ -223,7 +211,12 @@ const CONFIG = {
       class="text-primary" href="https://github.com/arifszn/gitprofile"
       target="_blank"
       rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+    >GitProfile</a> 
+    and <a class="text-primary" href="https://github.com/wowser20" 
+    target="_blank"
+    rel="noreferrer"
+    >GitProfile</a>
+    ❤️`,
 
   enablePWA: true,
 };
