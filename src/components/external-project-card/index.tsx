263e8c3829762a1bf915aca9aac09ff4b55618df
Andrea Ella Remoreras <ae.remoreras@gmail.com>
2026-02-19 20:56:45 +0800

    chore: update image

--- src/components/external-project-card/index.tsx
+++ src/components/external-project-card/index.tsx
@@ -99,7 +99,7 @@ const ExternalProjectCard = ({
                   </h2>
                   {item.imageUrl && (
                     <div className="avatar opacity-90">
-                      <div className="w-40 h-40 mask mask-hexagon">
+                      <div className="w-40 h-40 mask rounded-sm overflow-hidden">
                         <LazyImage
                           src={item.imageUrl}
                           alt={'thumbnail'}
